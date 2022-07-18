a43d56d19b348733602bc4240a1103fae0507e0b
LeTam9 <tamle5911@gmail.com>
2022-07-18 23:11:47 +0700

    second commit

--- js/company.js
+++ js/company.js
@@ -1,206 +1,137 @@
-const getInputs = container => [...container.querySelectorAll('.input')];
+var next_click=document.querySelectorAll(".next_button");
+var main_form=document.querySelectorAll(".main");
+var step_list = document.querySelectorAll(".progress-bar li");
+var num = document.querySelector(".step-number");
+let formnumber=0;
 
-//Get all fieldsets within the form element 
-
-const getFieldsets = container => [...container.querySelectorAll('fieldset')];
-
-//Get an Array with the distance of each fieldset element to apply a margin
-const getArrayOfMargins = (fieldsetsContainer) => {
-    return getFieldsets(fieldsetsContainer).map((fieldset, i) => fieldset.offsetHeight * i);
-
-};
-
-
-
-// Apply or add .focus class according if a element gain or loss its focus.
-let inpustEvent = container => {
-    
-    getInputs(container).map(input =>{
-    input.addEventListener("focusin" , e => {
-        let inputParent = e.target.parentElement;
-
-            if(!inputParent.classList.contains('focus')){
-                inputParent.classList.add('focus');
-            }
-        });
-        input.addEventListener("blur", e => {
-            let inputParent = e.target.parentElement;
-            if(input.required){
-                if(e.target.value == ""){
-                    inputParent.classList.replace('focus' , 'error');
-                } else {
-                    inputParent.classList.remove('error');
-                }
-            } else {
-                if (e.target.value == "") {
-                    inputParent.classList.remove('focus');
-                }
-            }
-                
-        });
-        input.addEventListener('keydown' , e => {
-            
-            if(e.key === "Enter" || e.key === "Tab") {
-                e.preventDefault();
-                if (e.key === "Enter" ) {
-                     container.querySelectorAll('.next')[0].click();
-                }
-               
-            }
-        });
-
-    
-    });
-};
-//Add function Navegation Prev and Next fr the fieldsets elements
-
-let formNavigation = (buttonsContainer, fieldsetsContainer, progressBarContainer,i) => {
-    let margins = getArrayOfMargins(fieldsetsContainer),
-        margin,
-        progressBarItems = [...progressBarContainer.querySelectorAll('li')];
-
-    
-    buttonsContainer.addEventListener("click" , e =>{
-
-        e.preventDefault();
-
-        let el = e.target,
-            getFieldsetsLenght = getFieldsets(fieldsetsContainer).length;
-
-        if(el.tagName === "BUTTON") {
-
-            if (el.classList.contains("prev")) {
-                
-             
-
-                        if (i > 0) { 
-                            margin = -margins[i-1];
-                            i--;
-                        } else {
-                            i =0;
-                        }
-
-                        fieldsetsContainer.style.marginTop = margin + "px";
-
-                 
-               
-            } else if (el.classList.contains("next") ){
-                
-                if (formValidation(i, fieldsetsContainer, progressBarContainer)) {
-
-                    if (i == (getFieldsetsLenght - 1) ) {
-
-                       // margin = margins[0];
-                        i = (getFieldsetsLenght - 1);
-                        document.querySelectorAll('.submit-button')[0].classList.add('active');
-
-                    } else {
-
-                        margin = -margins[i+1];
-                        i++;
-                        
-                    } 
-
-                }
-                fieldsetsContainer.style.marginTop = margin + "px" ;
-            }
-            
+next_click.forEach(function(next_click_form){
+    next_click_form.addEventListener('click',function(){
+        if(!validateform()){
+            return false
         }
-        progressBarItems.forEach(element => {
-            if (progressBarItems[i] === element){
-                element.classList.add('current');
-                if(element.classList.contains('complete')){
-                    element.classList.remove('current');
-                }
-            } else {
-                element.classList.remove('current');
-            }
-            
-        });
+       formnumber++;
+       updateform();
+       progress_forward();
+       contentchange();
     });
-    progressBar(progressBarContainer, fieldsetsContainer);
+}); 
 
-};
-
-
-//Add function navegation with the progress bar. 
-
-let progressBar = (progressBarContainer, fieldsetsContainer) => {
-
-    let links = [...progressBarContainer.querySelectorAll('a')],
-        margins = getArrayOfMargins(fieldsetsContainer);
-
-    progressBarContainer.addEventListener('click' , e => {
-        e.preventDefault();
-        if (e.target.tagName === "A" && (e.target.parentElement.classList.contains('complete') || e.target.parentElement.classList.contains('current'))){
-            let index = links.indexOf(e.target);            
-            fieldsetsContainer.style.marginTop = -margins[index] + "px";        
-        }
-
+var back_click=document.querySelectorAll(".back_button");
+back_click.forEach(function(back_click_form){
+    back_click_form.addEventListener('click',function(){
+       formnumber--;
+       updateform();
+       progress_backward();
+       contentchange();
     });
-};
-//Form Validation for each group of inputs within the fieldsets elements. Here you can add your own custom validation.
+});
 
-let formValidation = (i, fieldsetsContainer, progressBarContainer) => {
+var username=document.querySelector("#user_name");
+var shownname=document.querySelector(".shown_name");
+ 
 
-    let fieldsets = getFieldsets(fieldsetsContainer),
-        currentFieldset= fieldsets[i],
-        inputs = getInputs(currentFieldset);
+var submit_click=document.querySelectorAll(".submit_button");
+submit_click.forEach(function(submit_click_form){
+    submit_click_form.addEventListener('click',function(){
+       shownname.innerHTML= username.value;
+       formnumber++;
+       updateform(); 
+    });
+});
+
+var heart=document.querySelector(".fa-heart");
+heart.addEventListener('click',function(){
+   heart.classList.toggle('heart');
+});
 
 
-    for (const key in inputs) {
+var share=document.querySelector(".fa-share-alt");
+share.addEventListener('click',function(){
+   share.classList.toggle('share');
+});
 
-        let input = inputs[key];
-        if (input.required) {
-                if (input.value != ""){
+ 
 
-                input.parentElement.classList.remove('error');
-                continue;
+function updateform(){
+    main_form.forEach(function(mainform_number){
+        mainform_number.classList.remove('active');
+    })
+    main_form[formnumber].classList.add('active');
+} 
+ 
+function progress_forward(){
+    // step_list.forEach(list => {
+        
+    //     list.classList.remove('active');
+         
+    // }); 
+    
+     
+    num.innerHTML = formnumber+1;
+    step_list[formnumber].classList.add('active');
+}  
 
-            } else {
+function progress_backward(){
+    var form_num = formnumber+1;
+    step_list[form_num].classList.remove('active');
+    num.innerHTML = form_num;
+} 
+ 
+var step_num_content=document.querySelectorAll(".step-number-content");
 
-                console.dir(input.required);
-                input.parentElement.classList.add('error');
-                return false;
-
+ function contentchange(){
+     step_num_content.forEach(function(content){
+        content.classList.remove('active'); 
+        content.classList.add('d-none');
+     }); 
+     step_num_content[formnumber].classList.add('active');
+ } 
+ 
+ 
+function validateform(){
+    validate=true;
+    var validate_inputs=document.querySelectorAll(".main.active input");
+    validate_inputs.forEach(function(vaildate_input){
+        vaildate_input.classList.remove('warning');
+        if(vaildate_input.hasAttribute('require')){
+            if(vaildate_input.value.length==0){
+                validate=false;
+                vaildate_input.classList.add('warning');
             }
         }
-       
+    });
+    return validate;
+    
+}
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+        panel.style.display = "none";
+        } else {
+        panel.style.display = "block";
+        }
+    });
     }
-    progressBarContainer.querySelectorAll('li')[i].classList.add('complete');
-  
-    return true;
+    var add = document.getElementsByClassName("addnew");
+    var i;
 
-};
-
-//Submit button. Here you can add ajax request.
-let submit = (container,fieldsetsContainer) => {
-  let submitButton = container.querySelectorAll("input[type='submit']");
-  container.addEventListener('click' , e => {
-      if (e.target.type === "submit" && e.target.tagName === "INPUT" ) {
-        e.preventDefault();
-        let overlay = document.createElement('div');
-            overlay.classList.add('overlay');
-          overlay.innerHTML = ` <span class="load"></span>`;
-          container.appendChild(overlay);
-          setTimeout(() => {
-              overlay.innerHTML = ` <span class="success">✔ you have successfully registered</span>`;
-          }, 2000);
+    for (i = 0; i < acc.length; i++) {
+    add[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var addnew = this.nextElementSibling;
+        if (addnew.style.display === "block") {
+        addnew.style.display = "none";
+        } else {
+        addnew.style.display = "block";
+        }
+    });
     }
-  });
-   
-};
-
-
-const stepsforminit = container => {
-    let form = container,
-        fieldsetsContainer = container.querySelectorAll('#fieldset-container')[0],
-        progressBarContainer = container.querySelectorAll('#progress-bar')[0],
-        buttonsContainer = container.querySelectorAll('.tab-nav')[0],
-        i=0;
-    inpustEvent(form);
-    formNavigation(buttonsContainer, fieldsetsContainer, progressBarContainer,i);
-    submit(form, fieldsetsContainer);
-};
-
-stepsforminit(document.getElementById('form-wrapper'));
+    
+    
+        
+        
